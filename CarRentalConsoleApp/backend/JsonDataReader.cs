6946e80767e68f0d1cc87aa26123fbbe14a8681f
Marek Haładziński <160923199+marek-haladzinski@users.noreply.github.com>
2024-02-24 15:23:28 +0100

    24.02.2024 15:23

---
+++ CarRentalConsoleApp/backend/JsonDataReader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CarRentalConsoleApp.backend
+{
+    public static class JsonDataReader
+    {
+        public static List<Klient> GetKlients()
+        {
+            var path = Directory.GetCurrentDirectory() + "klienci.json";
+            var data = File.ReadAllText(path);
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            return JsonSerializer.Deserialize<List<Klient>>(data, options);
+        }
+
+        public static List<Samochod> GetSamochod()
+        {
+            var path = Directory.GetCurrentDirectory() + "samochody.json";
+            var data = File.ReadAllText(path);
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            return JsonSerializer.Deserialize<List<Samochod>>(data, options);
+        }
+    }
+}
---
+++ CarRentalConsoleApp/backend/klient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalConsoleApp.backend
+{
+    public class Klient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime License { get; set; }
+    }
+}
