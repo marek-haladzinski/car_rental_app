16d70f4ae6de2a0f99adc71bdf79d540512e5d0c
Marek Haładziński <160923199+marek-haladzinski@users.noreply.github.com>
2024-02-24 15:28:50 +0100

    24.02.2024 15:29

--- CarRentalConsoleApp/Program.cs
+++ CarRentalConsoleApp/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CarRentalConsoleApp.frontend;
+using CarRentalConsoleApp.backend;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    public static void Main()
+    {
+
+        Display.DisplayWelcome();
+
+        while (true)
+        {
+            string wybor = Console.ReadLine();
+
+            switch (wybor)
+            {
+                case "1":
+                    Display.DisplayKIS();
+                    break;
+
+                case "2":
+                    Console.WriteLine("Test2");
+                    break;
+
+                case "3":
+                    Environment.Exit(0);
+                    break;
+
+                default:
+                    Console.WriteLine("Nieprawidłowy wybór. Spróbuj ponownie.");
+                    break;
+            }
+
+            Display.DisplayWelcome();
+        }
+    }
+}
---
+++ CarRentalConsoleApp/frontend/Display.cs
@@ -0,0 +1,31 @@
+﻿using CarRentalConsoleApp.backend;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalConsoleApp.frontend
+{
+    public static class Display
+    {
+        public static void DisplayWelcome()
+        {
+            Console.WriteLine("MENU GŁÓWNE");
+            Console.WriteLine("1 -> LISTA KLIENTÓW I SAMOCHODÓW");
+            Console.WriteLine("2 -> WYPOŻYCZENIE SAMOCHODU");
+            Console.WriteLine("3 -> ZAKOŃCZ PROGRAM");
+            Console.WriteLine();
+            Console.WriteLine("Naciśnij 1, 2 lub 3 -> ...");
+        }
+
+        public static void DisplayKIS()
+        {
+            Console.WriteLine("Lista klientów:");
+            //foreach (var Klient in ListaKlientow)
+            //{
+            //Console.WriteLine($"ID: {Klient.Id}, Imię i nazwisko: {Klient}");
+            //}
+        }
+    }
+}
